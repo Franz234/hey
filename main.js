ecce8d81d7aeac35bd65eb3446f811658b0f5e9e
Glitch (hello-webpage) <none>
2022-11-05 14:59:18 +0000

    🖇⛲️ Checkpoint ./main.js:50687416/5745 ./index.html:50687416/78

--- main.js
+++ main.js
@@ -7,17 +7,49 @@ window.onload = () => {
   console.log("YOUR CODE GOES HERE");
 
   // Load the data set from the assets folder:
-  var width = 300;
-  var height = 300;
-  var svg = d3.select("#svgcontainer")
-  .append("svg")
-            .attr("width", width)
-            .attr("height", height);
-         svg.append("line")
-            .attr("x1", 100)
-            .attr("y1", 100)
-            .attr("x2", 200)
-            .attr("y2", 200)
-            .style("stroke", "rgb(255,0,0)")
-            .style("stroke-width", 2);
+  var svg = d3.select("svg"),
+            width = svg.attr("width"),
+            height = svg.attr("height"),
+            radius = Math.min(width, height) / 2;
+  var g = svg.append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  var color = d3.scaleOrdinal([
+            'gray', 'green', 'brown', 'orange', 'yellow', 'red', 'purple'
+         ]);
+  var pie = d3.pie().value(function(d) { 
+            return d.percent; 
+         });
+  var path = d3.arc()
+            .outerRadius(radius - 10).innerRadius(0);
+  var label = d3.arc()
+            .outerRadius(radius).innerRadius(radius - 80);
+  d3.csv("populations.csv", function(error, data) {
+            if (error) {
+               throw error;
+            }
+            console.log(data)
+            var arc = g.selectAll(".arc")
+               .data(pie(data))
+               .enter()
+               .append("g")
+               .attr("class", "arc");
+            
+            arc.append("path")
+               .attr("d", path)
+               .attr("fill", function(d) { return color(d.data.states); });
+        
+            console.log(arc)
+        
+            arc.append("text").attr("transform", function(d) { 
+               return "translate(" + label.centroid(d) + ")"; 
+            })
+            
+            .text(function(d) { return d.data.states; });
+         });
+  svg.append("g")
+            .attr("transform", "translate(" + (width / 2 - 120) + "," + 20 + ")")
+            .append("text").text("Top population states in india")
+            .attr("class", "title")
+  console.log("Siema")
+    
 };
