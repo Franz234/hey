6bae36ccdd3ce119cf98f705b9c52a49f5ac0a45
Glitch (hello-webpage) <none>
2022-11-06 13:23:14 +0000

    🍣🚜 Checkpoint ./main.js:50687416/3800

--- main.js
+++ main.js
@@ -23,22 +23,24 @@ window.onload = () => {
     "https://cdn.glitch.com/3406f498-ccaa-4592-93d3-c0a3a2e58c43%2Fcars.csv?v=1604907277091"
   ).then(function (data) {
     // Add X axis
-    console.log(data[0])
-    var x = d3.scaleLinear()
-    .domain([
-    	d3.min([0,d3.min(data,function (d) { return d['Horsepower(HP)'] })]),
-    	d3.max([0,d3.max(data,function (d) { return d['Horsepower(HP)'] })])
-    	])
-    .range([0, width]);
+    console.log(data);
+    var x = d3
+      .scaleLinear()
+      .domain([0,
+        d3.max([
+          d3.max(data, function (d) {
+            return d["Horsepower(HP)"];
+          }),
+        ]),
+      ])
+      .range([0, width]);
     svg
       .append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
     // Add Y axis
-    var y = d3.scaleLinear()
-    .domain([0, 80])
-    .range([height, 0]);
+    var y = d3.scaleLinear().domain([0, 80]).range([height, 0]);
     svg.append("g").call(d3.axisLeft(y));
 
     // Add dots
@@ -49,10 +51,10 @@ window.onload = () => {
       .enter()
       .append("circle")
       .attr("cx", function (d) {
-        return x(d['Horsepower(HP)']);
+        return x(d["Horsepower(HP)"]);
       })
       .attr("cy", function (d) {
-        return y(d['City Miles Per Gallon']);
+        return y(d["City Miles Per Gallon"]);
       })
       .attr("r", 3)
       .style("fill", "#69b3a2");
