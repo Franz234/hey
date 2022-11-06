ccf7e9a582d612ad8bdbd59de57089fc4a7af95f
Glitch (hello-webpage) <none>
2022-11-06 13:34:38 +0000

    🍀👦 Checkpoint ./main.js:50687416/3426

--- main.js
+++ main.js
@@ -23,24 +23,14 @@ window.onload = () => {
     "https://cdn.glitch.com/3406f498-ccaa-4592-93d3-c0a3a2e58c43%2Fcars.csv?v=1604907277091"
   ).then(function (data) {
     // Add X axis
-    console.log(data);
-    var x = d3
-      .scaleLinear()
-      .domain([0,
-        d3.max([
-          d3.max(data, function (d) {
-            return d["Horsepower(HP)"];
-          }),
-        ]),
-      ])
-      .range([0, width]);
+    var x = d3.scaleLinear().domain([0, 500]).range([0, width]);
     svg
       .append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
     // Add Y axis
-    var y = d3.scaleLinear().domain([0, 80]).range([height, 0]);
+    var y = d3.scaleLinear().domain([0, 70]).range([height, 0]);
     svg.append("g").call(d3.axisLeft(y));
 
     // Add dots
@@ -57,6 +47,6 @@ window.onload = () => {
         return y(d["City Miles Per Gallon"]);
       })
       .attr("r", 3)
-      .style("fill", "#69b3a2");
+      .style("fill", "green");
   });
 };
