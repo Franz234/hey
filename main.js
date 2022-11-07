64ef29e88ae6c108f26f9391a280221b0acf4525
Glitch (hello-webpage) <none>
2022-11-07 09:45:41 +0000

    💧☃️ Checkpoint ./style.css:50687416/407 ./main.js:50687416/8046

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ window.onload = () => {
 
     // Highlight the car type that is hovered
     var highlight = function (d) {
-      car_type = d["Type"];
+      var car_type = d["Type"];
 
       d3.selectAll(".dot")
         .transition()
@@ -87,5 +87,33 @@ window.onload = () => {
       })
       .on("mouseover", highlight)
       .on("mouseleave", doNotHighlight);
+
+    //Legend
+    var legend = svg
+      .selectAll(".legend")
+      .data(color.domain())
+      .enter()
+      .append("g")
+      .attr("class", "legend")
+      .attr("transform", function (d, i) {
+        return "translate(0," + i * 20 + ")";
+      });
+
+    legend
+      .append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+    legend
+      .append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function (d) {
+        return d;
+      });
   });
 };
