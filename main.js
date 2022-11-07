019db328d8cc625f6864f6062a2b86510996ad8a
Glitch (hello-webpage) <none>
2022-11-07 10:43:29 +0000

    🌜👡 Checkpoint ./main.js:50687416/36920 ./index.html:50687416/27

--- main.js
+++ main.js
@@ -23,31 +23,30 @@ window.onload = () => {
     "https://cdn.glitch.com/3406f498-ccaa-4592-93d3-c0a3a2e58c43%2Fcars.csv?v=1604907277091"
   ).then(function (data) {
     // Add X axis
-    var x = d3.scaleLinear().domain([0, 500]).range([0, width]).nice();
+    var x = d3.scaleLinear().domain([0, 500]).range([0, width]);
     svg
       .append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
     // Add Y axis
-    var y = d3.scaleLinear().domain([0, 70]).range([height, 0]).nice();
+    var y = d3.scaleLinear().domain([0, 70]).range([height, 0]);
     svg.append("g").call(d3.axisLeft(y));
 
-    //Define color encoding https://github.com/d3/d3-scale-chromatic
+    // Color scale: give me a car name, I return a color
     var color = d3
-      .sclaedOrdinal()
+      .scaleOrdinal()
       .domain(["Minivan", "Sedan", "Sports Car", "SUV", "Wagon"])
-      .range(d3.schemaTableu10);
-
+      .range(["red", "blue", "green", "black", "yellow"]);
 
     // Highlight the dot that is hovered
     var highlight = function (d) {
-      d3.select(this).attr("stroke", "#333").attr("stroke-width", 2);
+      d3.select(this).attr('stroke', '#333').attr('stroke-width', 2);
     };
 
     // Highlight the dot that is hovered
     var doNotHighlight = function () {
-      d3.select(this).attr("stroke", null);
+      d3.select(this).attr('stroke', null);
     };
 
     // Add dots
@@ -74,64 +73,32 @@ window.onload = () => {
       .on("mouseleave", doNotHighlight);
 
     //Legend
-    function colorLegend(container) {
-      const titlePadding = 14;
-      const entrySpacing = 16;
-      const entryRadius = 5;
-      const labelOffset = 4;
-      const baselineOffset = 4;
+    
+    var legend = svg
+      .selectAll(".legend")
+      .data(color.domain())
+      .enter()
+      .append("g")
+      .attr("class", "legend")
+      .attr("transform", function (d, i) {
+        return "translate(0," + i * 20 + ")";
+      });
 
-      const title = container
-        .append("text")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("fill", "black")
-        .attr("font-family", "Helvetica Neue, Arial")
-        .attr("font-weight", "bold")
-        .attr("font-size", "12px")
-        .text("Car type");
+    legend
+      .append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
 
-      const entries = container
-        .selectAll("g")
-        .data(color)
-        .join("g")
-        .attr(
-          "transform",
-          (d) => `translate(0, ${titlePadding + d.index * entrySpacing})`
-        );
-
-      const symbols = entries
-        .append("circle")
-        .attr("cx", entryRadius)
-        .attr("r", entryRadius)
-        .attr("fill", (d) => color(d.index));
-    }
-
-    //     var legend = svg
-    //       .selectAll(".legend")
-    //       .data(car.domain())
-    //       .enter()
-    //       .append("g")
-    //       .attr("class", "legend")
-    //       .attr("transform", function (d, i) {
-    //         return "translate(0," + i * 20 + ")";
-    //       });
-
-    //     legend
-    //       .append("rect")
-    //       .attr("x", width - 18)
-    //       .attr("width", 18)
-    //       .attr("height", 18)
-    //       .style("fill", color);
-
-    //     legend
-    //       .append("text")
-    //       .attr("x", width - 24)
-    //       .attr("y", 9)
-    //       .attr("dy", ".35em")
-    //       .style("text-anchor", "end")
-    //       .text(function (d) {
-    //         return d;
-    //       });
+    legend
+      .append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function (d) {
+        return d;
+      });
   });
 };
