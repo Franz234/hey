4a26c9fa8bfc828c3d1eeef39a113812c330fb2a
Glitch (hello-webpage) <none>
2022-11-10 10:29:12 +0000

    👛💧 Checkpoint ./main2.js:50687416/3505 ./index.html:50687416/1

--- main2.js
+++ main2.js
@@ -103,9 +103,92 @@ d3.csv(
         .text("Horsepower(HP)")
     svg.append("g")
         .classed("y axis", true)
-        .attr("transform", "translate(0," + height + ")")
         .call(yAxis)
-        .
+        .append("text")
+        .classed("label", true)
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left)
+        .attr("dy", "1.5em")
+        .style("text-anchor", "end")
+        .text("City Miles Per Gallon");
+    
+    var objects = svg.append("svg")
+        .classed("objects", true)
+        .attr("width", width)
+        .attr("height", height);
+    objects.append("svg:line")
+        .classed("axisLine hAxisLine", true)
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", width)
+        .attr("y2", 0)
+        .attr("transform", "translate(0," + height + ")");
+    objects.append("svg:line")
+        .classed("axisLine vAxisLine", true)
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", 0)
+        .attr("y2", height);
+    objects.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .classed("dot", true)
+        .attr({
+            r: function(d) {
+                return 4 * Math.sqrt(d[rCat] / Math.PI);
+            },
+            cx: function(d) {
+                return x(d[xCat]);
+            },
+            cy: function(d) {
+                return y(d[yCat]);
+            }
+        })
+    .style("fill", function(d) {
+        return color(d[colorCat]);
+    })
+        .on("mouseover", tip.show)
+        .on("mouseout", tip.hide);
+    
+     var legend = svg.selectAll(".legend")
+        .data(color.domain())
+        .enter().append("g")
+        .classed("legend", true)
+        .attr("transform", function(d, i) {
+            return "translate(0," + i * 20 + ")";
+        });
+    legend.append("rect")
+        .attr("x", width + 10)
+        .attr("width", 12)
+        .attr("height", 12)
+        .style("fill", color);
+    
+    legend.on("click", function(type) {
+        // dim all of the icons in legend
+        d3.selectAll(".legend")
+            .style("opacity", 0.1);
+        // make the one selected be un-dimmed
+        d3.select(this)
+            .style("opacity", 1);
+        // select all dots and apply 0 opacity (hide)
+        d3.selectAll(".dot")
+        // .transition()
+        // .duration(500)
+        .style("opacity", 0.0)
+        // filter out the ones we want to show and apply properties
+        .filter(function(d) {
+            return d["first_careunit"] == type;
+        })
+            .style("opacity", 1) // need this line to unhide dots
+        .style("stroke", "black")
+        // apply stroke rule
+        .style("fill", function(d) {
+            if (d.hospital_expire_flag == 1) {
+                return this
+            } else {
+                return "white"
+            };
+        });
     
     function zoom() {
         svg.select(".x.axis").call(xAxis);
